3a950777c8e9c7b5fb1086f38e6ef3b23de1cfdc
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-09-24 15:55:34 -0600

    Initial Commit.

---
+++ InventorySystem/InventorySystem/Models/Product .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Models
+{
+    /*
+        Product must have unique product “ID”
+        Product must have a product “Name”
+        Product must have a “Quantity” that is  greater than or equal to zero
+        Product must have an “IsDiscontinued” boolean
+        Set to false by default
+
+     */
+    public class Product
+    {
+    }
+}
